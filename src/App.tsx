2f4b6d0362d48b26e59a1b2a7d2e7f80494d0b58
Ricardo Olarte <raop155@hotmail.com>
2021-01-24 09:35:43 -0500

    Add Grid container & item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -25,10 +26,24 @@ const getProducts = async (): Promise<CartItemType[]> =>
 const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log(data);
+
+  const geTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveToCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
   return (
-    <div className='App'>
-      <h1>Start</h1>
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}></Item>{' '}
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 };
 
