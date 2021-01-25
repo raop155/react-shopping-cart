6e30acc6b0e55625b1a594e1a6978934accece38
Ricardo Olarte <raop155@hotmail.com>
2021-01-25 10:14:05 -0500

    Add Total in Cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -10,6 +10,9 @@ interface Props {
 }
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) => 
+  items.reduce((acc, item) => acc + item.amount * item.price , 0 )
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -17,6 +20,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
       {cartItems?.map((item) => (
         <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
       ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
